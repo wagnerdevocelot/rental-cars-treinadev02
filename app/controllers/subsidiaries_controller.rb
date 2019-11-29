9d7e0e23727fe1cb8f221b239f07c7f0aad72ccc
Joao Almeida <wagner.dev1337@gmail.com>
2019-11-29 00:29:02 -0300

    quarto rspec

---
+++ app/controllers/subsidiaries_controller.rb
@@ -0,0 +1,12 @@
+class SubsidiariesController < ApplicationController
+
+    def index
+        @subsidiaries = Subsidiary.all
+    end
+
+    def show
+        @subsidiaries = Subsidiary.find(params[:id])
+    end
+
+end
+
---
+++ spec/features/visitor_view_subsidiaries_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+feature 'Visitor view subsidiaries' do
+  scenario 'successfully' do
+    Subsidiary.create(name: 'Sao Paulo', cnpj: '55.110.650/0001-09',address: 'Alameda Santos')
+
+    visit root_path
+    click_on 'Filiais'
+    click_on 'Sao Paulo'
+    
+
+    expect(page).to have_content('Sao Paulo')
+    expect(page).to have_link('Voltar')
+  end
+
+  scenario 'and return to home page' do
+    Subsidiary.create(name: 'Sao Paulo')
+    Subsidiary.create(cnpj: '55.110.650/0001-09')
+    Subsidiary.create(address: 'Alameda Santos')
+
+    visit root_path
+    click_on 'Filiais'
+    click_on 'Sao Paulo'
+    click_on 'Voltar'
+    
+
+    expect(current_path).to eq root_path
+  end
+end
