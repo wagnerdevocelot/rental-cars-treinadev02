998c81aa07c79bb4c435c2ccb20581bb8318ec91
Joao Almeida <wagner.dev1337@gmail.com>
2019-11-28 23:03:40 -0300

    terceiro rspec

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -8,4 +8,21 @@ class ManufacturersController < ApplicationController
         @manufacturers = Manufacturer.find(params[:id])
     end
 
+    def new
+        @manufacturers = Manufacturer.new
+    end
+
+    def create
+        @manufacturers = Manufacturer.new(manufacturer_params)
+        @manufacturers.save
+        redirect_to @manufacturers
+    end
+
+    private
+
+    def manufacturer_params
+        params.require(:manufacturer).permit(:name)
+    end
+
+
 end
